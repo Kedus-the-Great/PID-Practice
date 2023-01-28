f688065b68e76e349467f40d8a8fb05515cb4379
Kedus-the-Great <kedus.f@prhsrobotics.com>
2023-01-28 10:07:24 -0500

    First Commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int LEFT_BACK_DRIVE_PORT = 1;
+    public static final int RIGHT_BACK_DRIVE_PORT = 2;
+    public static final int LEFT_FRONT_DRIVE_PORT = 3;
+    public static final int RIGHT_FRONT_DRIVE_PORT = 4;
+    public static final int CHASIS_IMU_PORT = 12;
+
+    public static WPI_TalonFX leftBackDriveMotor = new WPI_TalonFX(LEFT_BACK_DRIVE_PORT);
+    public static WPI_TalonFX rightBackDriveMotor = new WPI_TalonFX(RIGHT_BACK_DRIVE_PORT);
+    public static WPI_TalonFX leftFrontDriveMotor = new WPI_TalonFX(LEFT_FRONT_DRIVE_PORT);
+    public static WPI_TalonFX rightFrontDriveMotor = new WPI_TalonFX(RIGHT_FRONT_DRIVE_PORT);
+    public static PigeonIMU chasisIMU = new PigeonIMU(CHASIS_IMU_PORT);
+}
+}
